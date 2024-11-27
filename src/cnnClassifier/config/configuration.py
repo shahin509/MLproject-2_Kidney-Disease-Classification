079cf11591c2c028e888e3e1998cc4673499bbca
shahin509 <hossainshahin230@gmail.com>
2024-11-27 14:49:08 +0600

    mlflow added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -77,12 +77,11 @@ class ConfigurationManager:
     
 
 
-
     def get_evaluation_config(self) -> EvaluationConfig:
         eval_config = EvaluationConfig(
             path_of_model="artifacts/training/model.h5",
             training_data="artifacts/data_ingestion/kidney-ct-scan-image",
-            mlflow_uri="https://dagshub.com/entbappy/Kidney-Disease-Classification-MLflow-DVC.mlflow",
+            mlflow_uri="https://dagshub.com/shahin509/MLproject-2_Kidney-Disease-Classification.mlflow",
             all_params=self.params,
             params_image_size=self.params.IMAGE_SIZE,
             params_batch_size=self.params.BATCH_SIZE
